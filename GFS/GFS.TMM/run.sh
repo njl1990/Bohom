1020ec53fe57ac4debe4d5dce176c4df32dd282f
Bowen <bowen.nan@icloud.com>
2019-05-29 13:26:45 +0800

    update

--- GFS/GFS.TMM/run.sh
+++ GFS/GFS.TMM/run.sh
@@ -10,7 +10,7 @@ sudo docker rm gfs.tmm.web
 
 # run docker 
 #sudo docker run  --name=gfs.tmm.db -v /home/bowen/git/Bohom/GFS/GFS.TMM/db/:/data/db -d mongo
-sudo docker run  --name=gfs.tmm.web -v /home/bowen/git/Bohom/GFS/GFS.TMM/dev/:/home -itd -p 0.0.0.0:8888:8888 bw:web.0.1 '/bin/sh'
+sudo docker run  --name=gfs.tmm.web -v /home/bowen/git/Bohom/GFS/GFS.TMM/dev/:/home -id -p 0.0.0.0:8888:8888 bw:web.0.1 '/bin/sh'
 
 sudo docker exec -it gfs.tmm.web 'python3 --version'
 #sudo docker run --net=tmm-net --name=gfs.tmm.dc -p 0.0.0.0:234:234 -d gfs-tmm:dc-0.1
