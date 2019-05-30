663c31677aefed5061f90bb26503c09218f51a06
bowen <bowen.nan@icloud.com>
2019-05-30 21:18:28 +0800

    save data

---
+++ GFS/GFS.GLM/dev/cc/build.sh
@@ -0,0 +1,8 @@
+# stop docker 
+sudo docker stop gfs.glm.db
+# rm docker 
+sudo docker rm gfs.glm.db
+# run docker 
+sudo docker run --name=gfs.glm.db -p 0.0.0.0:27016:27017 -v /home/bowen/git/Bohum/GFS/GFS.TMM/db/:/data/db -d mongo
+echo start capture collection
+python cc.py
--- GFS/GFS.GLM/dev/cc/cc.py
+++ GFS/GFS.GLM/dev/cc/cc.py
@@ -4,51 +4,53 @@ import time
 import sys
 import cv2
 import shutil
-#from pymongo import MongoClient
+from pymongo import MongoClient
+from cStringIO import StringIO
+import bson.binary
 from PIL import Image
 
 
 def CatchUSBVideo(window_name,camera_idx):
+
+	#CaptureInterval=30*3600
+	CaptureInterval=5 # for test
+
 	cv2.namedWindow(window_name)
 	cap = cv2.VideoCapture(camera_idx)
+
+	mongoClient = MongoClient('0.0.0.0',27016)
+	DBClient = mongoClient['glmdb']
+	collection = DBClient['glmRcd']
+
 	while cap.isOpened():
+
+		filename = 'image.catch.jpg'
 		ok, frame = cap.read() 
-		cv2.imwrite('image.catch'+'.jpg',frame)
+		cv2.imwrite(filename,frame)
+		crtDate=time.strftime("%Y-%m-%d", time.localtime())
+		crtTime=time.strftime("%H:%M", time.localtime())
+
+
+		with open (filename,'rb') as image:
+			content = StringIO(image.read())
+			SaveData={
+	        	'data':bson.binary.Binary(content.getvalue()),
+				'date':crtDate,
+				'time':crtTime,
+			}
+			collection.save(SaveData)
+			print('SaveData:    date = '+SaveData['date']+'  time = '+ SaveData['time'])
+		os.remove('image.catch.jpg')
+		time.sleep(CaptureInterval)
+
 		break
 	cap.release()
 	cv2.destroyAllWindows()
 
-def CreateCaptureLog():
-	CatchUSBVideo('FaceRect',0)
-	crtDate=time.strftime("%Y-%m-%d", time.localtime())
-	crtTime=time.strftime("%H:%M", time.localtime())
-	CaptureLogData={
-		'path':'image.catch',
-		'date':crtDate,
-		'time':crtTime,
-	}
-	return CaptureLogData
-
-def SaveCaptureLog(CaptureLogData,num):
-	shutil.move('image.catch.jpg','./img/imge.catch-'+str(num)+'.jpg')
-
-def CleanCatch():
-	# clean catch file
-	os.remove('image.catch')
 
 ## Main
 def main():
-	#CaptureInterval=30*3600
-	CaptureInterval=5 # for test
-	i=0
-	while True:
-		CaptureLogData = CreateCaptureLog()
-		print(CaptureLogData)
-		i=i+1
-		SaveCaptureLog(CaptureLogData,i)
-		#CleanCatch()
-		#break
-		time.sleep(CaptureInterval)
+	CatchUSBVideo('cc',0)
 
 if __name__ == '__main__':
 	main()
