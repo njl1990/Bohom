4821596a1001b93b290ac18cfe3d06896a8ca7c5
bowen <bowen.nan@icloud.com>
2019-05-30 21:52:53 +0800

    change sh

--- GFS/GFS.GLM/dev/cc/build.sh
+++ GFS/GFS.GLM/dev/cc/build.sh
@@ -3,6 +3,7 @@ sudo docker stop gfs.glm.db
 # rm docker 
 sudo docker rm gfs.glm.db
 # run docker 
-sudo docker run --name=gfs.glm.db -p 0.0.0.0:27016:27017 -v /home/bowen/git/Bohum/GFS/GFS.TMM/db/:/data/db -d mongo
+sudo docker run --name=gfs.glm.db -p 0.0.0.0:27016:27017 -v /home/bowen/git/Bohom/GFS/GFS.GLM/db/:/data/db -d mongo
+
 echo start capture collection
 python cc.py
